a612cabeb3d70d07f86b25272e908f358b9d5de0
samirlefevre438-a11y <samirlefevre438@gmail.com>
2025-12-25 19:08:02 +0100

    test drive

--- server.js
+++ server.js
@@ -172,13 +172,15 @@ app.get("/kukipix", async (req, res) => {
       throw new Error("Variables d'environnement manquantes");
     }
 
-    const auth = new google.auth.GoogleAuth({
-      credentials: {
-        client_email: process.env.GOOGLE_CLIENT_EMAIL,
-        private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-      },
-      scopes: ["https://www.googleapis.com/auth/drive.readonly"],
-    });
+  const { google } = require("googleapis");
+
+const auth = new google.auth.GoogleAuth({
+  credentials: {
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  },
+  scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+});
 
     const drive = google.drive({ version: "v3", auth });
 
